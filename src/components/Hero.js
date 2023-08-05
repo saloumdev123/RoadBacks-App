504b44d9ea7216f4867e801168be5226a1eb2fba
papa saloum sene <saloumdev@gmail.com>
2023-08-05 19:37:03 +0000

    changing title

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ const Hero = () => {
     <div>
       <section className="hero" id="home">
         <div className="hero-banner">
-          <h1>continue exploring</h1>
+          <h1>Saloum-Service exploring</h1>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
             explicabo debitis est autem dicta.
